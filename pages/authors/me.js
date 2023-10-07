28f55276f1126434dcdaa982ae51bfe86544f97a
Valentin Olar <vali.olare@gmail.com>
2023-10-07 13:43:13 -0400

    adding pages

---
+++ pages/authors/me.js
@@ -0,0 +1,3 @@
+export default function Me() {
+    return <h1>I'm Val</h1>;
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Volar profile</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          Read <Link href="/posts/first-post">this page!</Link>
         </h1>
 
         <p className={styles.description}>
